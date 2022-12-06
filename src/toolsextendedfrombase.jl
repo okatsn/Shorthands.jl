c2fa6491ea68cbefcc268db3d3a3247445d679c3
okatsn <okatsn@gmail.com>
2022-12-06 16:00:00 +0800

    kwargs supports in round_step
    That is, round_step(x, step; kwargs...) for example

--- src/toolsextendedfrombase.jl
+++ src/toolsextendedfrombase.jl
@@ -1,8 +1,14 @@
 """
 `round_step(x, step)` round `x` to nearest `step`.
+`round_step` takes keyword arguments that `round` takes in rounding.
+
 # Example
 - `round_step(12.2, 0.25)` round to nearest 0.25
+- `round_step(12.2, 0.25; sigdigits=5)` is equivalent to `round(x / step; sigdigits=5) * step`
+
+
 """
-function round_step(x, step)
-    round(x / step) * step
+function round_step(x, step; kwargs...)
+    ans = round(x / step; kwargs...) * step
+    return ans
 end
--- test/toolsextendedfrombase.jl
+++ test/toolsextendedfrombase.jl
@@ -2,6 +2,8 @@
     testit = [
         (12.2, 0.25) => 12.25,
         (9527, 500)  => 9500.0,
+        (0.29, 0.25)  => 0.25,
+        (0.39, 0.25)  => 0.5,
     ]
     for ((x, step), ans) in testit
         @test round_step(x, step) == ans
