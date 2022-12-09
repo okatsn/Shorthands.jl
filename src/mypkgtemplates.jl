9b7bd08fd834104bcde9e40b1734fc1f9d1d5f63
okatsn <okatsn@gmail.com>
2022-12-09 12:03:51 +0800

    Read default README, append my msg, , and delete

--- src/mypkgtemplates.jl
+++ src/mypkgtemplates.jl
@@ -11,5 +11,35 @@ t("YourPackage")
 but with my configurations.
 """
 function ok_pkg_template()
+    dirtemp = mkpath("oktemptemp")
+    _dirtemp(args...) = joinpath(dirtemp, args...)
+    _dirtemp()
 
+    stdfile = PkgTemplates.Readme().file
+
+    dst = _dirtemp("OkREADME.md")
+    # cp(stdfile, _dirtemp("OkREADME1.md"); force=true) # original file
+
+    original = open(stdfile, "r") do io
+        read(io, String)
+    end
+
+    myreadme = """
+    $original
+
+    ## Instruction
+
+
+    """
+
+    open(dst, "a+") do io
+        write(io, myreadme)
+    end
+
+    PkgTemplates.Readme(; file=dst, destination="README.md")
+
+
+
+
+    rm(dst)
 end
