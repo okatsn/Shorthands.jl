eca4f3fbdbcddfef0111ae2962bb94d3e3ef2848
okatsn <okatsn@gmail.com>
2022-12-14 12:53:08 +0800

    Add custom changelog to ok_pkg_template

--- src/mypkgtemplates/mypkgtemplates.jl
+++ src/mypkgtemplates/mypkgtemplates.jl
@@ -42,10 +42,33 @@ function ok_pkg_template(yourpkgname::String; destination="", julia_ver = v"1.6"
         Codecov(), # https://about.codecov.io/
         Documenter{GitHubActions}(),
         PkgTemplates.Readme(; file=mypkgtemplate_dir("README.md"), destination="README.md"), # see PkgTemplates/src/plugins/readme.jl
-        TagBot(;registry="okatsn/OkRegistry"), # see PkgTemplates/src/plugins/tagbot.jl
-        # I use the template TagBot.yml in PkgTemplates directly.
-        # If your registry is public, this is all you need to do.
-        # For more information, see [here](https://github.com/JuliaRegistries/TagBot#custom-registries)
+        TagBot(;registry="okatsn/OkRegistry",
+                # If your registry is public, this is all you need to do.
+                # For more information, see [here](https://github.com/JuliaRegistries/TagBot#custom-registries)
+                changelog = """
+                ## {{ package }} {{ version }}
+                ${{github.event.head_commit.message}}
+                {% if previous_release %}
+                [Diff since {{ previous_release }}]({{ compare_url }})
+                {% endif %}
+                {% if custom %}
+                {{ custom }}
+                {% endif %}
+                {% if issues %}
+                **Closed issues:**
+                {% for issue in issues %}
+                - {{ issue.title }} (#{{ issue.number }})
+                {% endfor %}
+                {% endif %}
+                {% if pulls %}
+                **Merged pull requests:**
+                {% for pull in pulls %}
+                - {{ pull.title }} (#{{ pull.number }}) (@{{ pull.author.username }})
+                {% endfor %}
+                {% endif %}
+                """
+            ), # see PkgTemplates/src/plugins/tagbot.jl
+               # It use the template TagBot.yml in PkgTemplates directly, the mypkgtemplate/github/workflows/TagBot.yml is just for reference and can be deleted. `changelog` is modified from [default changelog template](https://github.com/JuliaRegistries/TagBot/blob/master/action.yml) with extra `${{github.event.head_commit.message}}`.
         Tests(; file=mypkgtemplate_dir("test","runtests.jl")) # see PkgTemplates/src/plugins/tests.jl
     ],
 
